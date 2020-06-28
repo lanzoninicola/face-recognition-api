58db5ae29d5fe95708de22d3b47acbf9105d19ec
Nicola <lanzoni.nicola@gmail.com>
2020-06-28 21:19:55 +0200

    first commit

--- server.js
+++ server.js
@@ -1,49 +1,82 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+let users = require('./users.js');
+
 
 // added security configuration
-require('./config/security')(app)
-
-// PERSONAL DATA
-const users = require('./users.json');
+// require('./config/security')(app)
 
 // PORT LISTENING
 app.set('port', process.env.PORT || 4000)
 
-
-// MIDDLEWARE FUNCTIONS
-const Logger = (req, res, next) => {
-    //console.log(req);
-    next();
-    return
-}
-
-const isAdmin = (req, res, next) => {
-    req.admin = true; // adding custom value to pass to the routes callbacks
-    console.log(users);
-    next();
-}
-
 // MIDDLEWARES
-app.use(Logger);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
 
 // ROUTES AND CALLBACKS
 
 app.get('/', (req, res) => {
-    res.send('hellooooooooodddoo!!') // sending unformatted text
+    res.send(users)
 });
 
-app.get('/admin', isAdmin, (req, res, next) => {
-    if (req.admin) {
-        res.json(users) // sending JSON file
+app.post('/signin', (req, res, next) => {
+
+    if (req.body.email === users[0].email &&
+        req.body.password === users[0].password) {
+        res.status(200).json("login successful")
     } else {
-        res.send('You are not authorized to view this content')
+        res.status(400).json("login failed")
     }
 });
 
+app.post('/register', (req, res, next) => {
+
+    const { name, email, password } = req.body;
+
+    if (name && email && password) {
+        users.push({
+            id: '188',
+            name: name,
+            email: email,
+            password: password
+        })
+
+        res.status(200).json('registration succesful');
+    } else {
+        res.status(500).json('some error occured');
+    }
+});
+
+app.get('/profile/:id', (req, res, next) => {
+
+    const { id } = req.params;
+
+    let userFound = users.filter(user => user.id === id);
+
+    if (userFound.length > 0) {
+        res.status(200).json('user found');
+    } else {
+        res.status(404).json('user not found');
+    }
+
+});
+
+app.post('/image', (req, res, next) => {
+
+    let { id } = req.body;
+
+    let userFound = users.filter(user => user.id === id);
+
+    console.log(userFound[0])
+
+    if (userFound.length > 0) {
+        userFound[0].entries++
+        res.status(200).json({ "entries": userFound[0].entries });
+    } else {
+        res.status(404).json('user not found');
+    }
+
+});
 
 app.listen(app.get('port'))
---
+++ users.js
@@ -0,0 +1,7 @@
+let users = [
+    { id: '123', name: 'Nicola', email: 'lanzoni.nicola@gmail.com', password: "mela", entries: 0, date: new Date() },
+    { id: '124', name: 'Mara', email: 'malacarnemara@gmail.com', password: "banana", entries: 0, date: new Date() }
+]
+
+
+module.exports = users;
